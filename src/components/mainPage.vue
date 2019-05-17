fd49a6635c1a1ac55bed84663aed9d00624c0397
unknown <1094050886@qq.com>
2019-05-17 12:34:52 +0800

    use element

--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -3,36 +3,11 @@
     <tabs></tabs>
   </div>
 </template>
-
 <script>
-import tabs from './tabs.vue'
+import tabs from "./tabs.vue"
 export default {
-  name: 'mainPage',
-  data() {
-    return {
-      message: "sjdksksd"
-    }
-  },
   components: {
     tabs
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/components/tabs.vue
+++ src/components/tabs.vue
@@ -1,62 +1,49 @@
 <template>
-    <div>
-        <div class="tabs">
-            <button v-for='tab in tabs' :key="tab.id" :class="{'is-active': tab.isActive}" @click='selectedTab(tab.id)'>
-                {{tab.name}}
-            </button>
-            <component :is="currentTabComponent"> </component>
-        </div>
-        <div class='tab-detail'>
-            <slot></slot>
-        </div>
-    </div>
+  <el-tabs v-model="currentTab">
+    <el-tab-pane v-for="tab in tabs" :key="tab.id" :label="tab.label">
+      <component :is="currentTabComponent"> </component>
+    </el-tab-pane>
+  </el-tabs>
 </template>
-
 <script>
 import tabBokeh from './tab-bokeh.vue'
 import tabCutout from './tab-cutout.vue'
 import tabBarrage from './tab-barrage.vue'
 export default {
-    name: 'tabs',
-    data() {
-        return {
-            currentTab: "bokeh", 
-            tabs: [
-                {
-                    id: "bokeh",
-                    name: "背景虚化",
-                    isActive: true
-                }, {
-                    id: "cutout",
-                    name: "抠图",
-                    isActive: false
-                }, {
-                    id: "barrage",
-                    name: "弹幕",
-                    isActive: false
-                }
-            ]
+  name: "tabs", 
+  data() {
+    return {
+      currentTab: 0, 
+      tabs: [
+        {
+          id: 0,
+          name: "bokeh",
+          label: "背景虚化",
+          isActive: true
+        }, {
+          id: 1,
+          name: "cutout",
+          label: "抠图",
+          isActive: false
+        }, {
+          id: 2,
+          name: "barrage",
+          label: "弹幕",
+          isActive: false
         }
-    },
-    computed: {
-        currentTabComponent: function() {
-            return 'tab-' + this.currentTab.toLowerCase()
-        }
-    },
-    methods: {
-        selectedTab: function(tab) {
-            this.currentTab = tab;
-        }
-    },
-    components: {
-        "tab-bokeh": tabBokeh,
-        "tab-cutout": tabCutout,
-        "tab-barrage": tabBarrage
+      ]
+    };
+  },
+  computed: {
+    currentTabComponent: function() {
+      return 'tab-' + this.tabs[this.currentTab].name.toLowerCase()
     }
+  },
+  components: {
+    "tab-bokeh": tabBokeh,
+    "tab-cutout": tabCutout,
+    "tab-barrage": tabBarrage
+  }
+};
 
-}
 </script>
-
-<style scoped>
-
-</style>
