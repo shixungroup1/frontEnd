c519722647e00905feaaafd829241de561958597
CurryYuan <CurryYuan@qq.com>
2019-05-18 16:55:59 +0800

    update pages

--- src/components/tab-cutout.vue
+++ src/components/tab-cutout.vue
@@ -1,15 +1,99 @@
 <template>
     <div>
-        This is cutout
+        <p>This is Cutout</p>
+        <div>
+            <el-image :src="url" class="img" :fit="fitMethod">
+                <div slot="error">
+                    <div class="im-slot">
+                        <span>请选择一张图片</span>
+                    </div>
+
+                </div>
+            </el-image>
+        </div>
+        <el-upload
+                class="upload-demo"
+                ref="upload"
+                action="https://jsonplaceholder.typicode.com/posts/"
+                :on-preview="handlePreview"
+                :on-remove="handleRemove"
+                :file-list="fileList"
+                list-type="picture-card"
+                :auto-upload="false">
+            <i class="el-icon-plus"></i>
+        </el-upload>
+        <div>
+            <el-button size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+        </div>
+        <div slot="tip" class="margin el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+
+        <div>
+            <el-button size="small" type="primary">下载图片</el-button>
+        </div>
+
+        <div>
+            <el-image :src="url1" class="img" :fit="fitMethod">
+                <div slot="error">
+                    <div class="im-slot">
+                        <span>后台处理中...</span>
+                    </div>
+                </div>
+            </el-image>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    
-}
+    import {get, post} from '../libs/http';
+    export default {
+        name: "tabCutout",
+        data() {
+            return{
+                fileList: [
+                    {name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'},
+                    {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}
+                ],
+                url: "",
+                url1:"",
+                fitMethod: 'contain'
+            }
+
+        },
+        methods: {
+            async getImage() {
+                let data={};
+                let res = await get('/get',data);
+                console.log(res)
+            },
+            submitUpload() {
+                this.$refs.upload.submit();
+            },
+            handleRemove(file, fileList) {
+                console.log(file, fileList);
+            },
+            handlePreview(file) {
+                console.log(file);
+                this.url=file.url;
+            }
+        }
+
+    }
 </script>
 
 <style scoped>
-
+    .img {
+        width: 400px;
+        height: 302px;
+        overflow: hidden;
+        margin: 10px;
+    }
+    .im-slot {
+        text-align: center;
+        line-height: 300px;
+        border: 1px dashed #c0ccda;
+        border-radius: 6px;
+    }
+    .margin {
+        margin: 10px;
+    }
 </style>
--- src/components/tabs.vue
+++ src/components/tabs.vue
@@ -1,49 +1,61 @@
 <template>
-  <el-tabs v-model="currentTab">
-    <el-tab-pane v-for="tab in tabs" :key="tab.id" :label="tab.label">
-      <component :is="currentTabComponent"> </component>
-    </el-tab-pane>
-  </el-tabs>
+    <el-tabs v-model="currentTab" :tab-position="tabPosition">
+        <el-tab-pane v-for="tab in tabs" :key="tab.id" :label="tab.label">
+            <component class="page" :is="currentTabComponent"></component>
+        </el-tab-pane>
+    </el-tabs>
 </template>
 <script>
-import tabBokeh from './tab-bokeh.vue'
-import tabCutout from './tab-cutout.vue'
-import tabBarrage from './tab-barrage.vue'
-export default {
-  name: "tabs", 
-  data() {
-    return {
-      currentTab: 0, 
-      tabs: [
-        {
-          id: 0,
-          name: "bokeh",
-          label: "背景虚化",
-          isActive: true
-        }, {
-          id: 1,
-          name: "cutout",
-          label: "抠图",
-          isActive: false
-        }, {
-          id: 2,
-          name: "barrage",
-          label: "弹幕",
-          isActive: false
+    import tabBokeh from './tab-bokeh.vue'
+    import tabCutout from './tab-cutout.vue'
+    import tabBarrage from './tab-barrage.vue'
+
+    export default {
+        name: "tabs",
+        data() {
+            return {
+                currentTab: 0,
+                tabPosition: 'left',
+                tabs: [
+                    {
+                        id: 0,
+                        name: "bokeh",
+                        label: "背景虚化",
+                        isActive: true
+                    }, {
+                        id: 1,
+                        name: "cutout",
+                        label: "抠图",
+                        isActive: false
+                    }, {
+                        id: 2,
+                        name: "barrage",
+                        label: "弹幕",
+                        isActive: false
+                    }
+                ]
+            };
+        },
+        computed: {
+            currentTabComponent: function () {
+                return 'tab-' + this.tabs[this.currentTab].name.toLowerCase()
+            }
+        },
+        components: {
+            "tab-bokeh": tabBokeh,
+            "tab-cutout": tabCutout,
+            "tab-barrage": tabBarrage
         }
-      ]
     };
-  },
-  computed: {
-    currentTabComponent: function() {
-      return 'tab-' + this.tabs[this.currentTab].name.toLowerCase()
-    }
-  },
-  components: {
-    "tab-bokeh": tabBokeh,
-    "tab-cutout": tabCutout,
-    "tab-barrage": tabBarrage
-  }
-};
 
 </script>
+
+<style scoped>
+    .page {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin-left: 10px;
+        align-items: center;
+    }
+</style>
