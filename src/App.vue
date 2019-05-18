e43b06029e0a5c8df14c63a0fb7f764286589867
unknown <1094050886@qq.com>
2019-05-18 09:50:28 +0800

    实现弹幕过人

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <main-page></main-page>
+    <main-page/>
   </div>
 </template>
 
@@ -10,7 +10,18 @@ import mainPage from './components/mainPage.vue'
 export default {
   name: 'app',
   components: {
-    'main-page': mainPage
+    "main-page": mainPage
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
